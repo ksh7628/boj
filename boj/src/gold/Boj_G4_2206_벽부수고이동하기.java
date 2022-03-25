b7b250ce1e9e85e89b039e37f079b212fd168ca2
Kim Seonhong <ksh7628@naver.com>
2022-03-25 21:59:38 +0900

    Boj_G4_2206_벽부수고이동하기 ref

--- boj/src/gold/Boj_G4_2206_벽부수고이동하기.java
+++ boj/src/gold/Boj_G4_2206_벽부수고이동하기.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 /**
  * @author	: KimSeonhong
- * @date	: 2021. 6. 9.
+ * @date	: 2022. 3. 25.
  *
  * 분류: 그래프 이론, 그래프 탐색, 너비 우선 탐색
  * 난이도: 골드4
@@ -17,10 +17,11 @@ import java.util.*;
  * 느낀 점: 
  * 예전에 정말 어렵게 느껴졌던 문제라서 다시 풀었는데 저번보다는 성능도 조금 향상되긴 했다.
  * 하지만 조건문을 더 사용한것과 3차원 배열을 사용해서 그런지 다른 사람들이 푼 코드에 비해 성능이 좋지 못했던 점이 아쉽다.
+ * 1차 리팩토링: 메모리 조금이라도 아끼기 위해 int -> char로 map을 선언, 큐에는 객체 3개만 담고 거리는 큐 사이클마다 증가하는 식으로 풀이.
  */
 public class Boj_G4_2206_벽부수고이동하기 {
-	static int[][] map;
-	static int[][][] dist;
+	static char[][] map;
+	static boolean[][][] visit;
 	static int[] dx = { -1, 1, 0, 0 };
 	static int[] dy = { 0, 0, -1, 1 };
 	static int N, M;
@@ -30,54 +31,67 @@ public class Boj_G4_2206_벽부수고이동하기 {
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		map = new int[N][M];
-		dist = new int[N][M][2];
+
+		map = new char[N][M];
+		visit = new boolean[2][N][M];
 
 		for (int i = 0; i < N; i++) {
-			String str = br.readLine();
-			for (int j = 0; j < M; j++) {
-				map[i][j] = str.charAt(j) - '0';
-			}
+			map[i] = br.readLine().toCharArray();
 		}
 
 		System.out.println(bfs());
 		br.close();
 	}
 
-	/* bfs를 통해 이동하면서 벽을 부쉈다는 상태를 3차원 배열의 인덱스로 나타냄  */
 	private static int bfs() {
-		Queue<int[]> q = new LinkedList<>();
-		dist[0][0][0] = 1;// 행, 열, 벽을 부순 횟수
+		ArrayDeque<int[]> q = new ArrayDeque<>();
+		visit[0][0][0] = true;
 		q.offer(new int[] { 0, 0, 0 });
+
+		// 시작하는 칸 포함해서 거리를 셈
+		int dist = 1;
 		while (!q.isEmpty()) {
-			int[] cur = q.poll();
-			if (cur[0] == N - 1 && cur[1] == M - 1) {
-				return dist[cur[0]][cur[1]][cur[2]];
-			}
-			for (int dir = 0; dir < 4; dir++) {
-				int nx = cur[0] + dx[dir];
-				int ny = cur[1] + dy[dir];
-				if (check(nx, ny) || dist[nx][ny][cur[2]] > 0) {
-					continue;
+			int size = q.size();
+
+			for (int i = 0; i < size; i++) {
+				int[] cur = q.poll();
+				if (cur[0] == N - 1 && cur[1] == M - 1) {
+					return dist;
 				}
 
-				if (map[nx][ny] == 1) {// 벽을 만났다면
-					if (cur[2] == 1 || (cur[2] == 1 && dist[nx][ny][cur[2] - 1] > 0)) {// 이미 벽을 부순적이 있다면 continue
+				for (int d = 0; d < 4; d++) {
+					int nx = cur[0] + dx[d];
+					int ny = cur[1] + dy[d];
+
+					if (!isCheck(nx, ny) || visit[cur[2]][nx][ny]) {
 						continue;
 					}
-					dist[nx][ny][cur[2] + 1] = dist[cur[0]][cur[1]][cur[2]] + 1;
-					q.offer(new int[] { nx, ny, cur[2] + 1 });// 벽을 부순 횟수 증가
-				} else {
-					dist[nx][ny][cur[2]] = dist[cur[0]][cur[1]][cur[2]] + 1;
-					q.offer(new int[] { nx, ny, cur[2] });
+
+					// 이미 벽을 부쉈는데 벽을 만나면 못부숨
+					if (cur[2] == 1 && map[nx][ny] == '1') {
+						continue;
+					}
+
+					// 이동 가능
+					if (map[nx][ny] == '0') {
+						visit[cur[2]][nx][ny] = true;
+						q.offer(new int[] { nx, ny, cur[2] });
+					// 벽을 부술 수 있고 부쉈는데 방문 안한 경우
+					} else if (cur[2] == 0 && !visit[1][nx][ny]) {
+						visit[1][nx][ny] = true;
+						q.offer(new int[] { nx, ny, 1 });
+					}
 				}
 			}
+
+			dist++;
 		}
+
 		return -1;
 	}
 
 	/* 배열 범위 체크 */
-	private static boolean check(int x, int y) {
-		return x < 0 || x >= N || y < 0 || y >= M;
+	private static boolean isCheck(int x, int y) {
+		return 0 <= x && x < N && 0 <= y && y < M;
 	}
 }
