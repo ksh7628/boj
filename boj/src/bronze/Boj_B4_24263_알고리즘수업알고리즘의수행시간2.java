81720947dd04bb3fd884e8b623b4d6fde298e3ed
ksh76 <ksh76@hong>
2024-09-05 23:51:18 +0900

    Boj_B4_24263_알고리즘수업알고리즘의수행시간2 sol

---
+++ boj/src/bronze/Boj_B4_24263_알고리즘수업알고리즘의수행시간2.java
@@ -0,0 +1,11 @@
+package bronze;
+
+import java.io.*;
+
+public class Boj_B4_24263_알고리즘수업알고리즘의수행시간2 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(Integer.parseInt(br.readLine()) + "\n1");
+		br.close();
+	}
+}
