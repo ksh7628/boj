a4f34af2af5d499bb282eb00e31ea74813f5b76c
ksh76 <ksh76@hong>
2024-09-05 23:36:08 +0900

    Boj_B5_27323_직사각형 sol

---
+++ boj/src/bronze/Boj_B5_27323_직사각형.java
@@ -0,0 +1,11 @@
+package bronze;
+
+import java.io.*;
+
+public class Boj_B5_27323_직사각형 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine()));
+		br.close();
+	}
+}
