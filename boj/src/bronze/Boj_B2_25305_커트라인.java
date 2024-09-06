301684a01f7147fbd310f41c82ea7e7445ec3459
ksh76 <ksh76@hong>
2024-09-07 03:48:19 +0900

    Boj_B2_25305_커트라인 sol

---
+++ boj/src/bronze/Boj_B2_25305_커트라인.java
@@ -0,0 +1,23 @@
+package bronze;
+
+import java.io.*;
+import java.util.*;
+
+public class Boj_B2_25305_커트라인 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr);
+		System.out.println(arr[N - k]);
+		br.close();
+	}
+}
